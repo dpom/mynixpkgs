6e34b261e8e484c3d5aacf819dfbad92afd1cd82
Dan Pomohaci <dan.pomohaci@gmail.com>
2024-11-16 12:32:24 +0200

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -3,24 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-      {
-        packages.${system} = rec {
-          cljstyle = pkgs.callPackage ./pkgs/cljstyle { };
-          dbeaver = pkgs.callPackage ./pkgs/dbeaver { };
-          rmview = pkgs.callPackage ./pkgs/rmview { };
-          combobulate = pkgs.emacsWithPackages(epkgs: [
-            (epkgs.callPackage ./pkgs/combobulate {
-              # inherit (pkgs) fetchFromGitHub;
-              # inherit (epkgs) melpaBuild;
-            })]);
-        };
-        templates = import ./templates/templates.nix;
+  outputs = { flake-parts, ... } @inputs:  flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      packages = {
+        cljstyle = pkgs.callPackage ./pkgs/cljstyle { };
+        dbeaver = pkgs.callPackage ./pkgs/dbeaver { };
+        rmview = pkgs.callPackage ./pkgs/rmview { };
       };
+    };
+    flake = {
+      templates = import ./templates/templates.nix;
+    };
+  };
 }
