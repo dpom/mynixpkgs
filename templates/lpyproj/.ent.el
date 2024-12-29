73a9cb5c3b64c75f8185d33a21914ce6caf7307b
Dan Pomohaci <dan.pomohaci@gmail.com>
2024-12-29 06:11:12 +0200

    add hooks to lpyproj

--- templates/lpyproj/.ent.el
+++ templates/lpyproj/.ent.el
@@ -6,7 +6,7 @@
 
 ;; project settings
 (setq ent-project-home (file-name-directory (if load-file-name load-file-name buffer-file-name)))
-(setq ent-project-name "cljproj")
+(setq ent-project-name "lpyproj")
 (setq ent-clean-regexp "~$\\|\\.tex$")
 
 (require 'ent)
@@ -18,8 +18,6 @@
 (task 'format '() "format code" '(lambda (&optional x) "bb format"))
 
 (task 'kondo '() "lint with kondo" '(lambda (&optional x) "bb kondo"))
-
-(task 'libupdate '() "search for new libs versions" '(lambda (&optional x) "bb libupdate" ))
 
 (task 'tests '() "run tests" '(lambda (&optional x) "bb test"))
 
